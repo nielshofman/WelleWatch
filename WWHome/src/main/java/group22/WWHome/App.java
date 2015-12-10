bf4a2a16d0c61c3c302268d3473dfc3dcbe2921b
Jens2 <j.vdmeer10@hotmail.com>
2015-12-10 15:42:10 +0100

    Heej hoi

---
+++ WWHome/src/main/java/group22/WWHome/App.java
@@ -0,0 +1,13 @@
+package group22.WWHome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
