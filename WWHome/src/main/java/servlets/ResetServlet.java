5c9bac55af22b3718553ea73b8765fecf78f35ae
Jens2 <j.vdmeer10@hotmail.com>
2015-12-14 13:51:22 +0100

    .

---
+++ WWHome/src/main/java/servlets/ResetServlet.java
@@ -0,0 +1,15 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class ResetServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResetServlet() {
+		super();
+	}
+}
