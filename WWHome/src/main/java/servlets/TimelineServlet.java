d9e7b0e11cfd9da01f223a1fac95f49325b0f4de
Jens2 <j.vdmeer10@hotmail.com>
2015-12-14 13:14:58 +0100

    boem

--- WWHome/src/main/java/servlets/TimelineServlet.java
+++ WWHome/src/main/java/servlets/TimelineServlet.java
@@ -1,5 +1,17 @@
 package servlets;
 
-public class TimelineServlet {
+import javax.servlet.http.HttpServlet;
 
+public class TimelineServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TimelineServlet() {
+		super();
+	}
+	
+	
 }
